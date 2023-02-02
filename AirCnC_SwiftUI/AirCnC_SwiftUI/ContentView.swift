e96247ff4ac222f5531d206228a5974cc60ffc3f
DongHyunTedKim <eastar29@naver.com>
2023-02-02 22:10:56 +0900

    add Step1

--- AirCnC_SwiftUI/AirCnC_SwiftUI/ContentView.swift
+++ AirCnC_SwiftUI/AirCnC_SwiftUI/ContentView.swift
@@ -2,24 +2,68 @@
 //  ContentView.swift
 //  AirCnC_SwiftUI
 //
-//  Created by Ted Kim on 2023/02/02.
+//  Created by Ted Kim on 2023/01/28.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading){
+            productImage
+            productDescription
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+private extension ContentView {
+    var productImage: some View {
+        ZStack{
+            Image("hattefjall")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+    
+    var productDescription: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("HATTEFJÄLL")
+                .font(.title)
+            HStack{
+                Image("franky")
+                    .resizable()
+                    .frame(width: 40, height: 40, alignment: .leading)
+                Text("프랭키")
+            }
+            HStack{
+                Text("가격")
+                    .frame(width: 60, alignment: .leading)
+                Text("10,000")
+            }
+            HStack{
+                Text("깊이")
+                    .frame(width: 60, alignment: .leading)
+                Text("68cm")
+            }
+            HStack{
+                Text("폭")
+                    .frame(width: 60, alignment: .leading)
+                Text("68cm")
+            }
+            HStack(){
+                Text("높이")
+                    .frame(width: 60, alignment: .leading)
+                Text("110cm")
+            }
+            Spacer()
+        }
+        .padding(8)
+    }
+}
+
+// MARK: - Previews
+
+struct ContentView_Preview: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Coder's High AirCnC Example
+
+## Step1 뷰
+
+  - 뷰 구성하기
+  - 레이아웃, 스택뷰
+  - 이미지와 애셋
+  - 텍스트, 이미지 콘텐츠 입력 
+
+<img src = "https://github.com/DongHyunTedKim/AirCnC_SwifUI/blob/Step1/Screenshot.png" width = "50%" height = "50%">  
