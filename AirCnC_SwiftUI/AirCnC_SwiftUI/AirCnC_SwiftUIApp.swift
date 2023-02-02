7b9f836c127ee34d3a889979bc57f57d81552727
DongHyunTedKim <eastar29@naver.com>
2023-02-02 22:06:17 +0900

    init Project

---
+++ AirCnC_SwiftUI/AirCnC_SwiftUI/AirCnC_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirCnC_SwiftUIApp.swift
+//  AirCnC_SwiftUI
+//
+//  Created by Ted Kim on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct AirCnC_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
