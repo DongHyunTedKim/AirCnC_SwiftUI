98efcfcf454a0384675a713c3f7ebc0b0d8b402b
DongHyunTedKim <eastar29@naver.com>
2023-02-07 18:29:16 +0900

    Step2 (1/2)
    - ZStack
    - LikeButton
    - Calender

--- AirCnC_SwiftUI/AirCnC_SwiftUI/ContentView.swift
+++ AirCnC_SwiftUI/AirCnC_SwiftUI/ContentView.swift
@@ -8,56 +8,132 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isSelected: Bool = false
+    @State private var date = Date()
+    
     var body: some View {
         VStack(alignment: .leading){
             productImage
             productDescription
+            reservationDate
         }
     }
 }
 
 private extension ContentView {
+    
     var productImage: some View {
-        ZStack{
+        ZStack{ // 1
             Image("hattefjall")
                 .resizable()
                 .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width*2/3)
+            HStack {
+                
+                // show prev image
+                Button {
+                } label: {
+                    Image("left_arrow")
+                }
+                
+                Spacer()
+                
+                // show next image
+                Button {
+                } label: {
+                    Image("right_arrow")
+                }
+
+            }
+            .padding()
         }
     }
     
     var productDescription: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Text("HATTEFJÄLL")
-                .font(.title)
+            HStack {
+                Text("HATTEFJÄLL")
+                    .font(.title)
+                
+                Spacer()
+                
+                LikeButton(isSelected: $isSelected)
+            }
+            
             HStack{
                 Image("franky")
                     .resizable()
                     .frame(width: 40, height: 40, alignment: .leading)
+                    .clipShape(Circle())
                 Text("프랭키")
+                    
             }
-            HStack{
-                Text("가격")
-                    .frame(width: 60, alignment: .leading)
-                Text("10,000")
+            VStack(spacing: 5){
+                HStack{
+                    HStack{
+                        Text("가격")
+                            .frame(width: 60, alignment: .leading)
+                        Text("34,000")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    HStack{
+                        Text("폭")
+                            .frame(width: 60, alignment: .leading)
+                        Text("86cm")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                HStack{
+                    HStack{
+                        Text("깊이")
+                            .frame(width: 60, alignment: .leading)
+                        Text("88cm")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    HStack(){
+                        Text("높이")
+                            .frame(width: 60, alignment: .leading)
+                        Text("101cm")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
             }
-            HStack{
-                Text("깊이")
-                    .frame(width: 60, alignment: .leading)
-                Text("68cm")
-            }
-            HStack{
-                Text("폭")
-                    .frame(width: 60, alignment: .leading)
-                Text("68cm")
-            }
-            HStack(){
-                Text("높이")
-                    .frame(width: 60, alignment: .leading)
-                Text("110cm")
-            }
-            Spacer()
         }
-        .padding(8)
+        .padding(20)
+    }
+    
+    var reservationDate: some View {
+        
+        VStack {
+            Text("\(date, formatter: dateFormatter)")
+            Form{
+                DatePicker("Date", selection: $date, in: Date()..., displayedComponents: [.date])
+                    .datePickerStyle(.wheel)
+                    .padding(10)
+            }
+        }
+        
+    }
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+}
+
+struct LikeButton: View {
+    @Binding var isSelected: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSelected.toggle()
+        }, label: {
+            Image(isSelected ? "btn_heart_filled" : "btn_heart_outline")
+        })
     }
 }
 
--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Coder's High AirCnC Example
 
-## Step1 뷰
+## Step2 인터랙티브
 
-  - 뷰 구성하기
-  - 레이아웃, 스택뷰
-  - 이미지와 애셋
-  - 텍스트, 이미지 콘텐츠 입력 
+  - 컨트롤과 컨트롤 액션
+  - 이벤트 다루기
+  - 컨트롤의 상태와 상태 활용하기
 
 <img src = "https://github.com/DongHyunTedKim/AirCnC_SwiftUI/blob/Step1/Screenshot.png" width = "50%" height = "50%">  
